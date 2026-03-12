5bfe5a40f4bee8c65e89845b33c14548cad88e05
KidWizardOfTheWeb <smasih2020@gmail.com>
2026-03-12 16:38:22 -0400

    Add requirements.txt, notes to client.py

--- localtests/SocketSelector/pyaudio/client.py
+++ localtests/SocketSelector/pyaudio/client.py
@@ -47,21 +47,23 @@ if modifiedMessage.decode() == "Connection allowed as listener.":
         pass
 else:
     # Send audio here
+    # TODO: Make sending data a thread.
+    # TODO: make receiving data from the server a thread.
+    # TODO: if someone else tries to connect to this channel:
+    # 1. send a message to the casting client and ask for permission to join.
+    # 2. If accepted, receive data from the co-casting user directly (think of the first caster as the host of all co-casters, they have their own server).
+    # 2a. Move the co-host status from just "cast" to "co-cast".
+    # 2b. Co-cast function will stream directly to the caster instead of the listener, while receiving data from main caster/other co-casters.
+    # 2c. We could let the server average all the packets maybe? But we cannot guarantee the packets will come in on the same thread in order to average them.
+    # 3. Combine audio input, normalize, send to listeners.
     try:
         while True:
+            # Implement another thread that receives audio data from co-caster input streams from the server.
+            # Server sends all co-caster data to the main caster on the channel, all audio is averaged and sent to server.
             data = input_stream.read(CHUNK)
             clientSocket.send(data)
     except KeyboardInterrupt as e:
         clientSocket.close()
         pass
-
-# for i in range (0, 3):
-#     # if i == 0:
-#     #
-#     # else:
-#         message = input('Type a sentence to send\n')
-#         clientSocket.send(message.encode())
-#         modifiedMessage = clientSocket.recv(1024)
-#         print(modifiedMessage.decode())
 
 clientSocket.close()
