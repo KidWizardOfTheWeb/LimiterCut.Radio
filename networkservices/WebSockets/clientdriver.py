2f91f4d9ffb82c380e13cb3053ee1ebe2f74aaae
KidWizardOfTheWeb <smasih2020@gmail.com>
2026-05-11 17:50:08 -0400

    Added CLI commands for clientdriver.py

--- networkservices/WebSockets/clientdriver.py
+++ networkservices/WebSockets/clientdriver.py
@@ -6,6 +6,7 @@ from environ import ImproperlyConfigured
 import environ # For reading environment variables
 from constants import ServerID
 import asyncio
+import argparse
 
 # Our imports
 import websockclient
@@ -26,7 +27,7 @@ except ImproperlyConfigured as e:
     serverName = "localhost"
 serverPort = 3601
 
-def request_a_channel():
+def request_a_channel(cli_args):
     # Note: change this later to not be hardcoded and allow this to retrieve name and ID from an API
     # server_names = [(s_names.name, s_names.value) for s_names in ServerID]
     # print("Available servers:")
@@ -37,9 +38,9 @@ def request_a_channel():
     # print("Available channels:\n"
     #       "10.24 <-> 655.35")
 
-    user_name = input("What's your username?\n")
-    channel_id = input("Request access to an available channel, or type the name to a new one: ")
-    channel_type = input("Request channel type (Radio, chat): ")
+    user_name = cli_args.Username if cli_args.Username else input("What's your username?\n")
+    channel_id = cli_args.Channel if cli_args.Channel else input("Request access to an available channel, or type the name to a new one: ")
+    channel_type = cli_args.Connecttype if cli_args.Connecttype else input("Request channel type (Radio, chat): ")
     print("Requesting channel access to the server and waiting for approval...")
 
     # TODO: Allow the user to choose a server first.
@@ -83,10 +84,38 @@ async def main_runner(json_req):
 # Any extra overarching async functionality you want to run as a new service goes here.
 if __name__ == "__main__":
     # Any init functionality should go before the asyncio run.
-    # TODO: allow args to be passed in instead of typing in CLI.
+    """
+    This should handle the following:
+    1. -GUI, --GUI (default OFF). Allows pyqt6 window to hook in for options.
+    2. -useconfig, --Useconfig (default OFF). Allows users to change things live, like inputs.
+    """
+    parser = argparse.ArgumentParser()
+
+    # Base info for connecting to the server, if the user chooses to add any of these flags on exec.
+    parser.add_argument("-username", "--Username",
+                        help="Enters a username to use when connecting to the server. If not provided, user is prompted in CLI instead.")
+    parser.add_argument("-channel", "--Channel",
+                        help="Enters a channel to connect to on a given server. If not provided, user is prompted in CLI instead.")
+    parser.add_argument("-connecttype", "--Connecttype",
+                        help="Chooses a connection type to a channel (radio/chat). If not provided, user is prompted in CLI instead.")
+
+    # Extra settings
+    parser.add_argument("-GUI", "--GUI", default=False, action='store_true', help="Turns GUI (windows instead of CLI) on or off. Default is OFF.")
+    parser.add_argument("-useconfig", "--Useconfig", default=False, action='store_true', help="Uses a config file for details on I/O and other settings, otherwise uses defaults. Default is OFF.")
+    args = parser.parse_args()
+
+    if args.GUI:
+        print("GUI: ON.")
+        # Add GUI hooks here (PyQt6 WIP).
+
+    if args.Useconfig:
+        print("Use config: ON")
+        # Add config ini hooks and loading here.
+        # configreader.py WIP.
+
 
     # Create json-request
-    json_req = json.dumps(request_a_channel())
+    json_req = json.dumps(request_a_channel(args))
 
     # Now, call our async services and run async tasks.
     try:
